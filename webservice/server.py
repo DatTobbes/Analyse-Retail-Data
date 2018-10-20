cf205bf5504f610bc0cb4f3dff81aa4b5262c886
Markus Glas <markus.glas@gmx.net>
2018-10-20 20:42:32 +0200

    add cors support

--- webservice/server.py
+++ webservice/server.py
@@ -1,6 +1,7 @@
 from flask import request
 import numpy as np
 import flask
+from flask_cors import cross_origin
 
 
 from data_aggregation.aggregate import DataFrameReader
@@ -11,9 +12,11 @@ app = flask.Flask(__name__)
 model = None
 
 @app.route("/devicedata", methods=["POST"])
+@cross_origin()
 def get_all_device_data():
     if flask.request.method == "POST":
         device = request.json.get('data')
+        print(device)
         device_data = device_data_reader.create_device_data(device)
 
         result = flask.jsonify(device_data)
